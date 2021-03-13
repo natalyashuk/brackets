6c2f32fc7bb82386a5c2e33e777fee5520aac6d3
natalyashuk <nata.lyashuk@gmail.com>
2021-03-13 07:54:33 +0200

    feat: add incomplete solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let count = 0
+       for (let i = 0; i < bracketsConfig.length; i++) {
+         if (bracketsConfig[i] == '(') {
+           count++;
+         }
+         else if (bracketsConfig[i] == ')') {
+          if (count == 0) {
+            return false
+           }
+               count--
+         }
+       }
+
+       if (count == 0) {
+         return true
+       }
+       return false
 }
